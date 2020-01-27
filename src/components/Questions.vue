4481f361f907dd95189be322b8beb58161b32a83
D0C702WH0 <jemyplu@gmail.com>
2020-01-27 15:49:55 +0100

    Displaying answers

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -5,8 +5,8 @@
         {{ currentQuestion.question }}
       </template>
       <hr class="my-4">
-      <p>
-        answers
+      <p v-for="answer in answers" :key="answer">
+        {{ answer }}
       </p>
       <b-button variant="primary" >Submit</b-button>
       <b-button @click="nextQuestion" variant="success" href="#">Next</b-button>
@@ -19,6 +19,14 @@ export default {
   props: {
     currentQuestion: Object,
     nextQuestion: Function
+  },
+  computed: {
+    answers() {
+      
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
   }
 }
 </script>
